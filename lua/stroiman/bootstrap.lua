3198607cc8c1db6b0a65e5521dca6cb38733f9e3
Peter Strøiman <peter@stroiman.com>
2025-05-04 15:55:04 +0200

    Move plugin installation helper

--- lua/stroiman/bootstrap.lua
+++ lua/stroiman/bootstrap.lua
@@ -1,20 +1,7 @@
-local M = {}
-
-M.install_plugin = function(path, name)
-  local pluginpath = vim.fn.stdpath("config") .. "/pack/vendor/opt/" .. name
-  local source = "https://github.com/" .. path
-  if not (vim.uv or vim.loop).fs_stat(pluginpath) then
-    vim.fn.system({
-      "git", "submodule", "add",
-      source,
-      pluginpath,
-    })
-  end
-end
-
 vim.cmd [[packadd gitsigns]]
 vim.cmd [[packadd catppuccin]]
 
+require("stroiman.plugins")
 require("stroiman.mason")
 require("stroiman.telescope")
 require("stroiman.fugitive")
---
+++ lua/stroiman/plugins.lua
@@ -0,0 +1,11 @@
+local install_plugin = function(path, name)
+  local pluginpath = vim.fn.stdpath("config") .. "/pack/vendor/opt/" .. name
+  local source = "https://github.com/" .. path
+  if not (vim.uv or vim.loop).fs_stat(pluginpath) then
+    vim.fn.system({
+      "git", "submodule", "add",
+      source,
+      pluginpath,
+    })
+  end
+end
