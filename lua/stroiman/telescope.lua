852df1e18116897d1f7b720c6d865c36a121e1d3
Peter Strøiman <peter@stroiman.com>
2025-05-05 23:17:18 +0200

    Check pwd for telescope file ignore patterns

--- lua/stroiman/telescope.lua
+++ lua/stroiman/telescope.lua
@@ -4,22 +4,18 @@ plugins.load("telescope")
 local builtin = require('telescope.builtin')
 local telescope = require('telescope')
 
-local pack_path = vim.fn.stdpath("config") .. "/pack"
-if not telescope.defaults then
-  telescope.defaults = {}
-end
-
-telescope.setup({
-  defaults = {
-    file_ignore_patterns = {
+vim.keymap.set('n', '<leader>ff', function()
+  local options = {}
+  -- If the current working folder is the neovim config folder, ignore files
+  -- from 3rd party plugins.
+  if vim.fn.getcwd() == vim.fn.stdpath("config") then
+    options.file_ignore_patterns = {
       "^pack/vendor/start/",
       "^pack/vendor/opt/",
-    },
-  }
-})
-
-
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    }
+  end
+  builtin.find_files(options)
+end, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
