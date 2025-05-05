541c74bef82427f3cbea2f404a77087868495f4d
Peter Strøiman <peter@stroiman.com>
2025-05-05 14:55:55 +0200

    Create command to install a plugin

--- lua/stroiman/plugins.lua
+++ lua/stroiman/plugins.lua
@@ -10,4 +10,23 @@ local install_plugin = function(path, name)
   end
 end
 
--- install_plugin("L3MON4D3/LuaSnip", "luasnip")
+--- @param plugin string The github project, e.g. stroiman/gotest.nvim
+local function get_folder_name(plugin)
+  local paths = vim.fn.split(plugin, "/")
+  if #paths ~= 2 then
+    print("Expected a string in the form user/repository")
+    return nil
+  end
+  local path = paths[#paths]
+  path = path:gsub("%.git", "")
+  path = path:gsub("%.nvim", "")
+  return path
+end
+
+vim.api.nvim_create_user_command("PlugInstall", function(cmd_args)
+  local path = cmd_args.args
+  local name = get_folder_name(path)
+  install_plugin(path, name)
+end, {
+  nargs = 1,
+})
