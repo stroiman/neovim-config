061511e448eafa8e5d4678b6ac6563ae2478af09
Peter Strøiman <peter@stroiman.com>
2025-05-06 11:23:17 +0200

    Refresh mason registry before installing

--- lua/stroiman/lsp/installer.lua
+++ lua/stroiman/lsp/installer.lua
@@ -10,6 +10,7 @@ local M = {}
 --- Instruct mason to install one or more tools.
 --- @param pkg string | string[]
 M.ensure_installed = function(pkg)
+  registry.refresh()
   local arg_type = type(pkg)
   if arg_type == "string" and not registry.is_installed(pkg) then
     print("Installing", pkg)
