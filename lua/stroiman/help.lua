bfa6e329f75dd7b6b95f6ac93a6c56b5008943b9
Peter Strøiman <peter@stroiman.com>
2025-05-06 15:26:45 +0200

    Take terminal width into consideration when moving help window

--- lua/stroiman/help.lua
+++ lua/stroiman/help.lua
@@ -1,24 +1,18 @@
 local group = vim.api.nvim_create_augroup("stroiman_help", {})
 
--- vim.api.nvim_create_autocmd("FileType", {
---   group = group,
---   pattern = "help",
---   callback = function()
---     vim.cmd([[
---       setlocal signcolumn=no
---       setlocal nonu
---     ]])
---   end,
--- })
-
 vim.api.nvim_create_autocmd("BufWinEnter", {
   group = group,
   callback = function(ev)
     local filetype = vim.fn.getbufvar(ev.buf, "&filetype")
     if filetype == "help" then
-      local winnr = vim.fn.winnr("$")
-      local winsize = vim.fn.winwidth(0)
-      if winnr < 3 and winsize >= 140 then
+      -- No of windows *including* the new help window
+      local no_of_windows = vim.fn.winnr("$")
+      local term_width = vim.o.columns
+      -- Reserve 80 columns for each window. Add 5 for gutter and window
+      -- separator. This doesn't compensate for horizontal splits - but let's
+      -- not go too crazy
+      local min_width = no_of_windows * 85
+      if term_width >= min_width then
         vim.cmd([[
           wincmd L
           vertical resize 80
