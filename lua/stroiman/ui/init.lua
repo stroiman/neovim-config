52fe19887fefbd32779676132e05c3489af3a712
Peter Strøiman <peter@stroiman.com>
2025-05-08 15:53:56 +0200

    Move help.lua to ui folder

--- lua/stroiman/ui/init.lua
+++ lua/stroiman/ui/init.lua
@@ -1,5 +1,7 @@
 local plugins = require("stroiman.plugins")
 
+require("stroiman.ui.help") -- Move the help window to the right, if there's enough space
+
 -- Smooth scrolling. I may keep it, I may throw it away.
 
 plugins.load("neoscroll")
