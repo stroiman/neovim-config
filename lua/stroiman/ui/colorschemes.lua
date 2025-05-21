3e9a43038683e5fdc01f936aebcef024213b98a1
Peter Strøiman <peter@stroiman.com>
2025-05-21 15:56:29 +0200

    Create keymap for changing light/dark prefs.

--- lua/stroiman/ui/colorschemes.lua
+++ lua/stroiman/ui/colorschemes.lua
@@ -9,3 +9,13 @@ require("gruvbox").setup({
 })
 
 vim.cmd.colorscheme("nightfox")
+
+-- UI changes
+vim.keymap.set("n", "<leader>ul", function()
+  vim.cmd.colorscheme("gruvbox")
+  vim.go.background = "light"
+end, { desc = "UI Light" })
+vim.keymap.set("n", "<leader>ud", function()
+  vim.cmd.colorscheme("nightfox")
+  vim.go.background = "dark"
+end, { desc = "UI Light" })
