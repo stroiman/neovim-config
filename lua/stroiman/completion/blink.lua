fe3cb03ffef968f5552620f92b0b681fddca38bb
Peter Strøiman <peter@stroiman.com>
2025-10-19 19:05:07 +0200

    Update and reconfigure blink

--- lua/stroiman/completion/blink.lua
+++ lua/stroiman/completion/blink.lua
@@ -3,8 +3,21 @@ plugins.load("blink.cmp")
 
 local blink = require("blink-cmp")
 blink.setup({
-  keymap = { preset = "default" },
+  keymap = {
+    preset = "default",
+    ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+    ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+
+    -- default in all keymap presets
+    ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+  },
+  completion = {
+    documentation = {
+      auto_show = true,
+    },
+  },
   appearance = {
     nerd_font_variant = "normal", -- mono or normal
   },
+  signature = { enabled = true },
 })
