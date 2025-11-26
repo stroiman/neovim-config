53660036ad281411bd532e9f2594d31408e13865
Peter Strøiman <peter@stroiman.com>
2025-11-26 17:17:21 +0100

    Fix gopls config, and enable jsonv2

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -3,7 +3,7 @@ return {
     gopls = {
       -- I run code that relies on the new *experimental* 1.24 synctest package,
       -- so this must be enabled through the ENV, or the LSP will complain.
-      env = { GOEXPERIMENT = "synctest" },
+      env = { GOEXPERIMENT = "jsonv2" },
     },
   },
 }
