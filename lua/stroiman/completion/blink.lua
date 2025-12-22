756f804aee3d91f45e6ad3837e8a204bbe1cb754
Peter Strøiman <peter@stroiman.com>
2025-12-22 22:38:57 +0100

    blink: Change keyboard map

--- lua/stroiman/completion/blink.lua
+++ lua/stroiman/completion/blink.lua
@@ -4,16 +4,42 @@ plugins.load("blink.cmp")
 local blink = require("blink-cmp")
 blink.setup({
   keymap = {
-    preset = "default",
-    ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-    ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+    preset = "none",
+    ["<C-u>"] = { "scroll_signature_up", "scroll_documentation_up", "fallback" },
+    ["<C-d>"] = { "scroll_signature_down", "scroll_documentation_down", "fallback" },
+    --
+    -- -- default in all keymap presets
+    ["<C-k>"] = { "snippet_forward", "show_signature", "hide_signature", "fallback" },
+    ["<C-h>"] = { "snippet_backward", "show_signature", "hide_signature", "fallback" },
+    ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+    ["<C-e>"] = { "hide", "fallback" },
+    ["<C-y>"] = { "select_and_accept", "fallback" },
 
-    -- default in all keymap presets
-    ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+    ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+    ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
+    ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+    ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+    -- ["<C-k>"] = false,
+    -- ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
   },
+  snippets = { preset = "luasnip" },
   completion = {
+    -- ghost_text = {
+    --   enabled = true,
+    -- },
     documentation = {
       auto_show = true,
+    },
+    menu = {
+      -- nvim-cmp style menu
+      draw = {
+        columns = {
+          { "label", "label_description", gap = 1 },
+          { "kind_icon", "kind" },
+        },
+      },
     },
   },
   appearance = {
