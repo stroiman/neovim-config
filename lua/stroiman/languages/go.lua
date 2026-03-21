8e856059ae28ef29c3cfccecc63e0f568f6b8fcb
Peter Strøiman <peter@stroiman.com>
2026-03-21 18:05:43 +0100

    Run templ when saving templ file

--- lua/stroiman/languages/go.lua
+++ lua/stroiman/languages/go.lua
@@ -1,4 +1,6 @@
 vim.cmd.packadd("gotest")
+local group = vim.api.nvim_create_augroup("stroiman.languages.go", { clear = true })
+
 local features = require("stroiman.features")
 local formatters = require("stroiman.languages.formatters")
 
@@ -49,6 +51,14 @@ local projections = require("stroiman.navigation.projectionist")
 -- The .h/.cc files are for working with v8go. Maybe extract this to a
 -- separate configuration
 projections.add_projection("go.mod", {
+  ["*.templ"] = {
+    command = "templ",
+    alternate = { "{}_templ.go" },
+  },
+  ["*_templ.go"] = {
+    command = "templ_go",
+    alternate = { "{}.templ" },
+  },
   ["*.go"] = {
     command = "src",
     alternate = {
@@ -79,5 +89,34 @@ projections.add_projection("go.mod", {
 
 vim.lsp.enable("gopls")
 vim.lsp.enable("clangd")
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = group,
+  pattern = "*.templ",
+  callback = function(event)
+    local stdout = {}
+    local stderr = {}
+    local jobId = vim.fn.jobstart({ "go", "tool", "templ", "generate", "-f", event.file }, {
+      stderr_buffered = true,
+      stdout_buffered = true,
+      on_stdout = function(_, data)
+        stdout = data
+      end,
+      on_stderr = function(_, data)
+        stderr = data
+      end,
+      on_exit = function(_, exitcode)
+        if exitcode > 0 then
+          print(vim.inspect({
+            "Templ error",
+            exitcode = exitcode,
+            stdout = stdout,
+            stderr = stderr,
+          }))
+        end
+      end,
+    })
+  end,
+})
 
 return M
